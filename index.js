d5cc6d30f1d3ae45a1b1fd4b5550b624a1545d44
tanya-ananya <taaruni.ananya@gmail.com>
2024-11-04 23:07:34 -0500

    version 4

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // TODO: Query for button with an id "theme-button"
-
+let themeButton = document.getElementById("theme-button")
 
 // TODO: Complete the toggleDarkMode function
 const toggleDarkMode = () => {
-
-    // Write your code to manipulate the DOM here
-
-
+    document.body.classList.toggle("dark-mode");
 }
 
-
 // TODO: Register a 'click' event listener for the theme button
+themeButton.addEventListener("click", toggleDarkMode);
+
 // Set toggleDarkMode as the callback function.
